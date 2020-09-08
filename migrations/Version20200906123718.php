29f62c643e8b4c5007c742b1879243097020e871
ilovehacking92 <mafarfafaoua@insea.ac.ma>
2020-09-08 17:44:41 +0100

    adding the whole project

---
+++ migrations/Version20200906123718.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200906123718 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE vente (id INT AUTO_INCREMENT NOT NULL, id_vente VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, id_produit VARCHAR(255) NOT NULL, category_produit VARCHAR(255) NOT NULL, type_produit VARCHAR(255) NOT NULL, prix_unit_produit DOUBLE PRECISION NOT NULL, nom_produit VARCHAR(255) NOT NULL, description_produit LONGTEXT DEFAULT NULL, designation_produit VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE commande (id INT AUTO_INCREMENT NOT NULL, id_commande VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE achat (id INT AUTO_INCREMENT NOT NULL, id_achat VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE achat');
+        $this->addSql('DROP TABLE commande');
+        $this->addSql('DROP TABLE produit');
+        $this->addSql('DROP TABLE vente');
+    }
+}
